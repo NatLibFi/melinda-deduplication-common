653decd6ab3cc3df9d3ff2a47e30733d740e7ac2
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-07-19 14:44:02 +0300

    Adds implementation for the datastore connector

--- types/data-store-service.flow.js
+++ types/data-store-service.flow.js
@@ -5,6 +5,6 @@ import type { DuplicateCandidate } from './duplicate-candidate.flow';
 
 export type DataStoreService = {
   loadRecord: (base: string, recordId: string) => Promise<MarcRecord>,
-  addRecord: (base: string, recordId: string, record: MarcRecord) => Promise<any>,
+  saveRecord: (base: string, recordId: string, record: MarcRecord) => Promise<any>,
   getDuplicateCandidates: (base: string, recordId: string) => Promise<Array<DuplicateCandidate>>
 };
--- utils/data-store-service.js
+++ utils/data-store-service.js
@@ -1,20 +1,47 @@
 // @flow
 import type { DataStoreService } from 'types/data-store-service.flow';
+const fetch = require('node-fetch');
+const MarcRecord = require('marc-record-js');
 
-function createDataStoreService(): DataStoreService {
+const logger = require('./logger');
+logger.log('info', 'Starting melinda-deduplication-listener');
 
-  function addRecord(base, recordId) { 
-    return Promise.reject('TODO'); 
+function createDataStoreService(datastoreAPI: string): DataStoreService {
+
+  async function saveRecord(base, recordId, record) {
+    
+    const url = `${datastoreAPI}/record/${base}/${recordId}`;
+    logger.log('info', `Saving record to url: ${url}`);
+    logger.log('info', `Record:\n${record.toString()}`);
+    const result = await fetch(url, { 
+      method: 'PUT', 
+      body: JSON.stringify(record),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    if (result.status !== 200) {
+      throw new Error(result.statusText);
+    }
   }
-  function loadRecord(base, recordId, record) { 
-    return Promise.reject('TODO');
+
+  async function loadRecord(base, recordId) { 
+    const url = `${datastoreAPI}/record/${base}/${recordId}`;
+    logger.log('info', `Loading record from url: ${url}`);
+    const result = await fetch(url);
+    const json = await result.json();
+    const record = new MarcRecord(json);
+    return record;
   }
-  function getDuplicateCandidates(base, recordId) {
-    return Promise.reject('TODO');
+
+  async function getDuplicateCandidates(base, recordId) {
+    const url = `${datastoreAPI}/candidates/${base}/${recordId}`;
+    logger.log('info', `Loading duplicate candidate set from url: ${url}`);
+    const result = await fetch(url);
+    const json = await result.json();
+    return json;
   }
 
   return {
-    addRecord,
+    saveRecord,
     loadRecord,
     getDuplicateCandidates
   };
