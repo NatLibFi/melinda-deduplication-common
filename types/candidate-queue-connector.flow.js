bdfaae29bc58ec37e34b2bfcdca535c42fc94371
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-08-11 13:07:20 +0300

    Missed that.

--- types/candidate-queue-connector.flow.js
+++ types/candidate-queue-connector.flow.js
@@ -5,7 +5,7 @@ import type { DuplicateCandidate } from './duplicate-candidate.flow';
 export type CandidateDoneCallback = () => mixed;
 export type OnCandidate = (candidate: DuplicateCandidate, done: CandidateDoneCallback) => mixed;
 
-export type CandidateQueueService = {
+export type CandidateQueueConnector = {
   pushCandidates: (duplicateCandidates: Array<DuplicateCandidate>) => Promise<any>,
   listenForCandidates: (onCandidate: OnCandidate) => Promise<any>
 };
