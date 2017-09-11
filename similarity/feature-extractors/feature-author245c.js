30ef14b3b81e45dbf89090dd2eb88a776ddf8c66
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-11 13:13:35 +0300

    Fixes bugs in author 245c handling

--- similarity/feature-extractors/feature-author245c.js
+++ similarity/feature-extractors/feature-author245c.js
@@ -1,5 +1,6 @@
 const compareFuncs = require('./core.compare');
 const { Labels } = require('./constants');
+const _ = require('lodash');
 
 const {
   normalize,
@@ -27,7 +28,7 @@ function author245c(record1, record2) {
       fields: [fields1, fields2],
       normalized: [normalized1, normalized2]
     };
-  }
+  }  
 
   function check() {
 
@@ -43,10 +44,40 @@ function author245c(record1, record2) {
       return Labels.SURE;
     }
 
-    if (compareFuncs.isIdentical(set1, set2, compareFuncs.stringPartofComparator)) {
-      debug('isIdentical stringPartofComparator');
+    const getValue = (set) => _.get(set, '[0].subfield', []).map(sub => sub._).join(' ');
+    const isSubset = (subset, superset) => _.difference(subset, superset).length === 0;
+    const isIdentical = (set1, set2) => isSubset(set1, set2) && isSubset(set2, set1);
+    const generateAbbrevations = (str) => str.split(' ').map((word, index, arr) => {
+      const abbreviation = word.substr(0,1);
+      return _.concat(arr.slice(0,index), abbreviation, arr.slice(index+1) ).join(' ');
+    });
+
+    const name1 = getValue(set1);
+    const name2 = getValue(set2);
+    
+    const words1 = name1.split(' ');
+    const words2 = name2.split(' ');
+    
+    const abbrevations1 = generateAbbrevations(name1);
+    const abbrevations2 = generateAbbrevations(name2);
+
+    const comparisons = _.concat(
+      abbrevations1.map(abbr => [abbr, name2]),
+      abbrevations2.map(abbr => [abbr, name1])
+    );
+
+    if (comparisons.some(([name1, name2]) => name1 === name2)) {
       return Labels.SURE;
-    }			
+    }
+
+    if (isSubset(words1, words2) || isSubset(words2, words1)) {
+      return Labels.ALMOST_SURE;
+    }
+	
+    if (compareFuncs.isIdentical(set1, set2, compareFuncs.lvComparator(0.80))) {
+      debug('isIdentical lvComparator.80');
+      return Labels.SURE;
+    }
 
     if (compareFuncs.isIdentical(set1, set2, compareFuncs.jaccardComparator(0.66))) {
       debug('isIdentical jaccardComparator0.66');
