2b83e09fa7b2329906bf4b314b26b4703de838a1
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-11 15:21:05 +0300

    Adds support for composite extractors

--- similarity/similarity.js
+++ similarity/similarity.js
@@ -15,12 +15,17 @@ function extractFeatures(strategy, record1, record2) {
 
 function executeFeatureExtractors(strategy, record1, record2) {
   
-  return strategy.map(function(extractorDefinition) {
+  return _.flatMap(strategy, function(extractorDefinition) {
     const Extractor = FeatureExtractors[extractorDefinition.name];
     const extractorInstance = new Extractor(record1, record2);
     
     const featureSimilarity = extractorInstance.check();
     
+    if (_.isArray(featureSimilarity)) {
+      const names = extractorInstance.names;
+      return _.zip(names, featureSimilarity).map(([name, similarity]) => ({name, similarity}));
+    }
+
     return {
       name: extractorDefinition.name,
       similarity: featureSimilarity
