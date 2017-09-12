cec17710c9c91a85ab8139a6d82dad9276a5b8c8
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-12 10:44:32 +0300

    Adds createLogger for instantiating multiple loggers

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,17 @@
 const winston = require('winston');
 const moment = require('moment');
 
-var logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp: () => moment().format('YYYY-MM-DDTHH:mm:ss.SSSZ')
-    })
-  ]
-});
+function createLogger() {
+  return new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        timestamp: () => moment().format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+      })
+    ]
+  });
+}
 
+
+const logger = createLogger();
+logger.createLogger = createLogger;
 module.exports = logger;
