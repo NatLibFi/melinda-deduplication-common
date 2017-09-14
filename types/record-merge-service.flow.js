a787be1b11997f9edca6236de0e7595a5e4b62ae
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-14 15:10:54 +0300

    fix

--- types/record-merge-service.flow.js
+++ types/record-merge-service.flow.js
@@ -1,6 +1,5 @@
 // @flow
 
-import type { RankedPair } from './preferred-record-service.flow';
 import type { MarcRecord } from './marc-record.flow';
 
 export type RecordIdentifier = {
@@ -8,18 +7,17 @@ export type RecordIdentifier = {
   base: string
 };
 
-export type MergeResult = {
-  record: RecordIdentifier
+export type RecordFamily = {
+  record: MarcRecord,
+  subrecords: Array<MarcRecord>
 };
 
-export type RankedSubrecords = {
-  preferredSubrecords: Array<MarcRecord>,
-  otherSubrecords: Array<MarcRecord>
-}
-export type RankedPairWithSubrecords = RankedPair & RankedSubrecords;
+export type MergeResult = {
+  mergedRecordFamily: RecordFamily
+};
 
 export type MergeabilityClass = 'NOT_MERGEABLE' | 'MANUALLY_MERGEABLE' | 'AUTOMATICALLY_MERGEABLE';
 
 export type RecordMergeService = {
-  mergeRecords: (recordPair: RankedPairWithSubrecords) => Promise<MergeResult>
+  mergeRecords: (preferredRecordFamily: RecordFamily, otherRecordFamily: RecordFamily) => Promise<MergeResult>
 };
--- utils/record-merge-service.js
+++ utils/record-merge-service.js
@@ -6,6 +6,8 @@ import type { Logger } from 'types/logger.flow';
 const _ = require('lodash');
 const createRecordMerger = require('@natlibfi/marc-record-merge');
 const PostMerge = require('melinda-deduplication-common/marc-record-merge-utils/marc-record-merge-postmerge-service');
+const createComponentRecordMatchService = require('./component-record-match-service.js');
+const MergeValidation = require('melinda-deduplication-common/marc-record-merge-utils/marc-record-merge-validate-service');
 
 const MergeabilityClass = {
   NOT_MERGEABLE: 'NOT_MERGEABLE',
@@ -13,16 +15,30 @@ const MergeabilityClass = {
   AUTOMATICALLY_MERGEABLE: 'AUTOMATICALLY_MERGEABLE'
 };
 
-function createRecordMergeService(mergeConfiguration: any, melindaConnector: MelindaRecordService, logger: Logger): RecordMergeService {
+function createRecordMergeService(
+  mergeConfiguration: any, 
+  componentRecordMatcherConfiguration: any, 
+  melindaConnector: MelindaRecordService, 
+  logger: Logger
+): RecordMergeService {
   
   async function mergeRecords(preferredRecordFamily, otherRecordFamily) {
 
     const merge = createRecordMerger(mergeConfiguration);
+    const componentRecordMatcher = createComponentRecordMatchService(componentRecordMatcherConfiguration);
     
+    const RecordValidationRules = MergeValidation.preset.melinda_host_automerge;
     const postMergeFixes = PostMerge.preset.defaults;
+
+    const componentRecordValidationRules = MergeValidation.preset.melinda_component;
+    const componentPostMergeFixes = _.clone(PostMerge.preset.defaults);
 
     try {
    
+      const preferredRecord = preferredRecordFamily.record;
+      const otherRecord = otherRecordFamily.record;
+
+      await MergeValidation.validateMergeCandidates(RecordValidationRules, preferredRecord, otherRecord);
       const mergedRecord = await merge(preferredRecord, otherRecord);
       const result = await PostMerge.applyPostMergeModifications(postMergeFixes, preferredRecord, otherRecord, mergedRecord);
       
@@ -31,82 +47,38 @@ function createRecordMergeService(mergeConfiguration: any, melindaConnector: Mel
       const preferredRecordId = selectRecordId(preferredRecord);
       const otherRecordId = selectRecordId(otherRecord);
   
-      const saveRecordOptions = { bypass_low_validation: 1, handle_deleted: 1, no_rerouting: 1, bypass_index_check: 1 };
-
-      // remove old ones
-
-      try {
-        logger.log('info', `Removing ${preferredRecordId}`);
-
-        preferredRecord.appendField(['STA', '', '', 'a', 'DELETED']);
-        updateRecordLeader(preferredRecord, 5, 'd');
-
-        const res = await melindaConnector.saveRecord('fin01', preferredRecordId, preferredRecord, saveRecordOptions);
-        logger.log('info', `Delete ok for ${preferredRecordId}`, res.messages);
-      } catch(error) {
-        logger.log('info', 'Failed to delete record', error);
-        throw error;
-      }
-
-      try {
-        logger.log('info', `Removing ${otherRecordId}`);
-
-        otherRecord.appendField(['STA', '', '', 'a', 'DELETED']);
-        updateRecordLeader(otherRecord, 5, 'd');
-
-        const res = await melindaConnector.saveRecord('fin01', otherRecordId, otherRecord, saveRecordOptions);
-        logger.log('info', `Delete ok for ${otherRecordId}`, res.messages);
-      } catch(error) {
-        logger.log('info', 'Failed to delete record', error);
-        
-        await undeleteRecord(melindaConnector, preferredRecordId);
-
-        throw error;
-      }
-
-      // add new one
-
-      try {
-        logger.log('info', 'Saving merged record.');
-        const res = await melindaConnector.createRecord('fin01', fixedMergedRecord, saveRecordOptions);
-        logger.log('info', `Save ok. New record is ${res.recordId}`, res.messages);
-        return {
-          record: {
-            base: 'FIN01',
-            id: res.recordId
-          }
-        };
-        
-      } catch(error) {
-        logger.log('info', 'Failed to add merged record', error);
-        await undeleteRecord(melindaConnector, preferredRecordId);
-        await undeleteRecord(melindaConnector, otherRecordId);
-
-        throw error;
-      }
+      const preferredSubrecordList = preferredRecordFamily.subrecords;
+      const otherSubrecordList = otherRecordFamily.subrecords;
       
+      const matchedSubrecordPairs = componentRecordMatcher.match(preferredSubrecordList, otherSubrecordList);
+
+      if (!validateSubrecordSets(matchedSubrecordPairs)) {
+        throw new MergeValidation.MergeValidationError('subrecord sets are not equal');
+      }
+     
+      const preferredHostRecordId = preferredRecordId;
+      const otherHostRecordId = otherRecordId;
+
+      // insert select773 just before sort (see: componentPostMergeFixes definition from PostMerge presets )
+      componentPostMergeFixes.splice(componentPostMergeFixes.length-1, 0, PostMerge.select773Fields(preferredHostRecordId, otherHostRecordId));
+
+      let mergedSubrecords = [];
+      for (const pair of matchedSubrecordPairs) {
+        const [preferredRecord, otherRecord] = pair;
+
+        await MergeValidation.validateMergeCandidates(componentRecordValidationRules, preferredRecord, otherRecord);
+        const mergedRecord = await merge(preferredRecord, otherRecord);
+        const result = await PostMerge.applyPostMergeModifications(postMergeFixes, preferredRecord, otherRecord, mergedRecord);
+        mergedSubrecords.push(result);
+      }
+
+      return {
+        record: fixedMergedRecord,
+        subrecords: mergedSubrecords.map(res => res.record)
+      };
+
     } catch(error) {
       throw error;
-    }
-  }
-
-
-  async function undeleteRecord(melindaConnector, recordId) {
-    const base = 'fin01';
-    
-    logger.log('info', `Undeleting ${recordId}`);
-    try {
-      const record = await melindaConnector.loadRecord(base, recordId, { handle_deleted:1, no_rerouting: 1 });
-
-      record.fields = record.fields.filter(field => field.tag !== 'STA');
-      updateRecordLeader(record, 5, 'c');
-      const res = await melindaConnector.saveRecord(base, recordId, record, {bypass_low_validation: 1, handle_deleted: 1, no_rerouting: 1, bypass_index_check: 1});
-
-      logger.log('info', `Undelete ok for ${recordId}`, res.messages);
-      return res;
-    } catch(err) {
-      logger.log('error', 'Failed to undelete record', err);
-      throw err;
     }
   }
 
@@ -115,13 +87,17 @@ function createRecordMergeService(mergeConfiguration: any, melindaConnector: Mel
   };
 }
 
+function validateSubrecordSets(matchedSubrecordPairs) {
+  return matchedSubrecordPairs.every(([a,b]) => {
+    if (a === undefined || b === undefined) {
+      return false;
+    }
+    return true;
+  });
+}
 
 function selectRecordId(record) {
   return _.get(record.fields.find(field => field.tag === '001'), 'value');
-}
-
-function updateRecordLeader(record, index, characters) {
-  record.leader = record.leader.substr(0,index) + characters + record.leader.substr(index+characters.length);
 }
 
 module.exports = {
