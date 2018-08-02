6584b69f0234b937265c5efade729ca178f857d3
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2018-08-02 12:01:39 +0300

    Change start-stop-timer export

--- src/utils/index.js
+++ src/utils/index.js
@@ -42,7 +42,7 @@ import * as DuplicateDatabaseConnector from './duplicate-database-connector';
 import * as RecordMergeService from './record-merge-service';
 import * as DataStoreConnector from './datastore-connector';
 import Logger from './logger';
-import StartStopTimer from './start-stop-timer';
+import createTimer from './start-stop-timer';
 import createComponentRecordMatchService from './component-record-match-service';
 
 export {
@@ -61,6 +61,6 @@ export {
   RecordMergeService,
   DataStoreConnector,
   Logger,
-  StartStopTimer,
+  createTimer,
   createComponentRecordMatchService
 };
